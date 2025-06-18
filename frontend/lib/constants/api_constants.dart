63682fc8cf23eb2494a18dd561ea2676b9768bcc
Minsu Seo <lullulalal@gmail.com>
2025-06-19 00:09:37 +0200

    Seperate frontend and backend

--- frontend/lib/constants/api_constants.dart
+++
@@ -1,6 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = 'http://127.0.0.1:8000';
-
-  static const String colorTransfer = '$baseUrl/colorTransfer';
-  static const String noiseRemover = '$baseUrl/noiseRemover';
-}
