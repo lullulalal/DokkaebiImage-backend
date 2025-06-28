2c20fea328b63a39dfbc00b8daa4b8575ef2a14d
Minsu Seo <lullulalal@gmail.com>
2025-06-28 23:16:32 +0200

    Change the folder structure Add root api to test

--- app/main.py
+++ app/main.py
@@ -9,11 +9,18 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://lullulalal.github.io",  # ✅ GitHub Pages 도메인
+    ],
+    allow_origin_regex=r"http://localhost:\d+",          # ✅ 포트가 바뀌는 로컬 개발환경
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from DokkaebiImage backend!"}
 
 app.include_router(color_transfer.router)
 app.include_router(noise_remover.router)
