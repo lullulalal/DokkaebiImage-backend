65981583f201e7f6621f04d3b29424b1177e815e
Minsu Seo <lullulalal@gmail.com>
2025-06-29 03:51:38 +0200

    Deployment

---
+++ certbot-renew.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# remove "certbot" container
+docker rm -f certbot 2>/dev/null
+
+# update let's encrypt certification
+docker run --rm --name certbot \
+-v '/var/www/DokkaebiImage-backend/docker/certbot/conf:/etc/letsencrypt' \
+-v '/var/www/DokkaebiImage-backend/docker/certbot/log:/var/log/letsencrypt' \
+-v '/var/www/DokkaebiImage-backend/docker/certbot/www:/var/www/certbot' \
+certbot/certbot certonly --webroot -w /var/www/certbot --force-renewal \
+--server https://acme-v02.api.letsencrypt.org/directory \
+--cert-name 138.199.215.9.nip.io &&
+
+# restart ngins
+docker exec nginx nginx -s reload
